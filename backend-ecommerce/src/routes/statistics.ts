1b98727bc0933f9d0a37d28cbb8ace560e2f09b2
Manas Kumar Jena <pixeldesignindia2023@gmail.com>
2024-01-18 12:33:11 +0530

    piechART INTIGRATION

--- backend-ecommerce/src/routes/statistics.ts
+++ backend-ecommerce/src/routes/statistics.ts
@@ -3,6 +3,8 @@ import express from "express";
 const app = express.Router();
 
 import { adminOnly } from "../middlewares/auth.js";
-import { getDashboardStats } from "../controllers/statistics.js";
+import { getDashboardStats, getPieCharts } from "../controllers/statistics.js";
 app.get("/dashboardStats",adminOnly,getDashboardStats)
+
+app.get("/pieChart",adminOnly,getPieCharts)
 export default app;
