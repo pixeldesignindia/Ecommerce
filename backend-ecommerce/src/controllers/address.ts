b7634a895bc67910a34c752753663da60b869ee9
Manas Kumar Jena <pixeldesignindia2023@gmail.com>
2024-02-03 11:58:09 +0530

    refactor:- adresss issue

--- backend-ecommerce/src/controllers/address.ts
+++ backend-ecommerce/src/controllers/address.ts
@@ -13,14 +13,15 @@ export const getAllAdressByUser =TryCatch(async(req,res,next)=>{
         let getAllAdress=[];
   
     if(!user) return next(new ErrorHandler("please provide  userId in the params ", 400));
-  if (myCache.has(key))
+  if (myCache.has(key)){
     getAllAdress = JSON.parse(myCache.get(key) as string);
-else{
+  
+  }else{
   getAllAdress  =  await Address.find({user:user,isDeleted:false}).sort({createdAt:-1});
-    if(getAllAdress.length<1) return next(new ErrorHandler("You have no Address yet", 400));
+  if(getAllAdress.length<1) return next(new ErrorHandler("You have no Address yet", 400));
      myCache.set(key, JSON.stringify(getAllAdress));
 }
-      return res.status(200).json({
+     return res.status(200).json({
     success: true,
     getAllAdress,
   });
@@ -31,13 +32,12 @@ export const deleteAdress = TryCatch(async (req, res, next) => {
 
   const address = await Address.findById(id);
   if (!address) return next(new ErrorHandler("address  Not Found", 404));
-
-  await Address.updateOne(
+await Address.updateOne(
     { _id:id },
     { $set: { isDeleted: true } }
   );
   invalidateCache({
-    addressId:String(address._id),
+    addressId:String(id),
     shippingAddress:true,
     userId: address.user,
     
